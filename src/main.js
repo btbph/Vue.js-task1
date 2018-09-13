4be5d8511a27ded6cad5d2e1782d63457f9a8b56
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2018-09-13 12:25:56 +0300

    finish first task

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import ShowName from './components/ShowName'
 
+Vue.component('ShowName', ShowName)
 Vue.config.productionTip = false
 
 new Vue({
